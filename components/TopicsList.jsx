11543d6857e70182df96ca53c26229a28be04959
thesurajdas <suraj4apps@gmail.com>
2024-11-24 13:16:42 +0530

    fix error and dynamic routes to components

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -4,31 +4,28 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const getTopics = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/topics", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST_URL}/api/topics`, {
       cache: "no-store",
     });
 
     if (!res.ok) {
       throw new Error("Failed to fetch topics");
     }
-
     return res.json();
-
   } catch (error) {
-    console.log("Error loading topics: ", error);
+    return {};
   }
 };
 
 export default async function TopicsList() {
-  const {topics} = await getTopics();
-
+  const data = await getTopics();
+  const topics = data.topics;
   return (
     <>
-      {topics.map((t) => (
+      {topics?.map((t) => (
         <div
           key={t._id}
-          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start rounded-md"
-        >
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start rounded-md">
           <div>
             <h2 className="font-bold text-2xl">{t.title}</h2>
             <div>{t.description}</div>
